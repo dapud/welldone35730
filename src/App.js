f79305f9f4b3bdcab80c0d476ab047fb7cb798ef
davidapud <dapud@hotmail.com.ar>
2022-08-20 13:02:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <h1> Hola mundooo !!!</h1>
+   <h2> holaaa</h2>
+   <li>lkjashdas</li>
+   <li>lkjashdas</li>
+   <li>lkjashdas</li>
+   <li>lkjashdas</li>
+   <li>lkjashdas</li>
+   <li>lkjashdas</li>
+   
+
+
+   </>
   );
 }
 
